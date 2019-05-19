fff4ad06e9ced90510dce25f8b22a68e5cf5d88b
Gkurin <gonzalokurin@gmail.com>
2019-05-19 16:52:04 -0300

    UserException

---
+++ TuBarrioServer/TuBarrio.Exceptions/UserException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TuBarrio.Exceptions
+{
+    [Serializable]
+    public class UserException : Exception
+    {
+        public UserException()
+        {
+        }
+
+        public UserException(string message) : base(message)
+        {
+        }
+
+        public UserException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UserException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
