b13da6816657bcd2d568f3808229773e1fefe1fc
Gonzalo <gonzalo@moonideas.com>
2019-05-20 16:38:54 -0300

    Added InvalidCredentialException

---
+++ TuBarrioServer/TuBarrio.Entities/EncodedImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuBarrio.Entities
+{
+    public class EncodedImage
+    {
+
+        public int Id { get; set; }
+        public string Image { get; set; }
+
+        public EncodedImage() { }
+        public EncodedImage(string image)
+        {
+            Image = image;
+        }
+
+    }
+}
---
+++ TuBarrioServer/TuBarrio.Exceptions/InvalidCredentialException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TuBarrio.Exceptions
+{
+    [Serializable]
+    public class InvalidCredentialException : Exception
+    {
+        public InvalidCredentialException()
+        {
+        }
+
+        public InvalidCredentialException(string message) : base(message)
+        {
+        }
+
+        public InvalidCredentialException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidCredentialException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
