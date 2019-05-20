bafbdfd4afa947cc5c2b956c88754dd2f1dd7ae4
Gonzalo <gonzalo@moonideas.com>
2019-05-20 16:39:12 -0300

    Google Sign in

---
+++ TuBarrioServer/TuBarrio.BusinessLogic/AuthenticationLogic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuBarrio.Entities;
+using TuBarrio.Exceptions;
+using TuBarrio.Repository;
+
+namespace TuBarrio.BusinessLogic
+{
+    public class AuthenticationLogic : IAuthenticationLogic
+    {
+        private IUserRepository userRepository;
+
+        public User GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserWithToken(string tokenRecieved)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HandleGoogleSignIn(string email, string name, string surname)
+        {
+            User user = userRepository.GetUserByEmail(email);
+            if (user == null)
+            {
+                user = new User(name, surname, email, "123", new Guid().ToString(), new EncodedImage(""));
+                userRepository.AddUser(user);
+            }
+            return LogIn(user.Email, user.Password);
+        }
+
+        public string LogIn(string email, string password)
+        {
+            string returnToken = userRepository.LogIn(email, password);
+            if (returnToken == "")
+            {
+                throw new InvalidCredentialException("No se encontro un usuario con esas credenciales");
+            }
+            else
+            {
+                return returnToken;
+            }
+        }
+    }
+}
---
+++ TuBarrioServer/TuBarrio.BusinessLogic/UserLogic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuBarrio.Entities;
+using TuBarrio.EntityModels;
+using TuBarrio.Repository;
+
+namespace TuBarrio.BusinessLogic
+{
+    public class UserLogic : IUserLogic
+    {
+        private IUserRepository userRepository;
+
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUserToFriends(string token, User friend)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetFriends(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserFromModel(UserModel user)
+        {
+            User newUser = new User(user.Name, user.Surname, user.Email, user.Phone, user.Password, new EncodedImage(user.ProfileImage));
+            return newUser;
+        }
+
+        public bool IsUserAlreadyExisting(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetDeviceNotificationToken(string token, string deviceToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            userRepository.UpdateUser(user);
+        }
+    }
+}
