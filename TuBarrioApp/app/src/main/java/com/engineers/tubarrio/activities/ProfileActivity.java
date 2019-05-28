91f8b955866a8978983376000cdf9c4e7b62b75f
Nicolas Damiani <nico_damiani@hotmail.com>
2019-05-28 16:02:04 -0300

    design elements in profile

---
+++ TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.engineers.tubarrio.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.engineers.tubarrio.R;
+
+public class ProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
