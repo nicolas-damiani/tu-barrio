53e53c887729e625c945589c105f464bcb7356c2
Nicolas Damiani <nico_damiani@hotmail.com>
2019-06-02 17:09:17 -0300

    main layout created

---
+++ TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/PublicationsActivity.java
@@ -0,0 +1,15 @@
+package com.engineers.tubarrio.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.engineers.tubarrio.R;
+
+public class PublicationsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publications);
+    }
+}
