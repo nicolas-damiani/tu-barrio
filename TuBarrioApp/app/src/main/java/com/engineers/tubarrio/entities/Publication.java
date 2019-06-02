4105e3cff1d4ed6cd51877985e2088f4c1427407
Nicolas Damiani <nico_damiani@hotmail.com>
2019-06-02 17:44:51 -0300

    publications adapter created

---
+++ TuBarrioApp/app/src/main/java/com/engineers/tubarrio/entities/Publication.java
@@ -0,0 +1,23 @@
+package com.engineers.tubarrio.entities;
+
+public class Publication {
+
+    private String title;
+    private String fileUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+}
