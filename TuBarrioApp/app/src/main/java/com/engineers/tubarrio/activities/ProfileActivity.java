a0e3205df30df40703d45191b5cec2fc0d4e0cb8
Sebastian <srodriguez@moonideas.com>
2019-06-02 19:34:29 -0300

    Profile activity

--- TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/ProfileActivity.java
+++ TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/ProfileActivity.java
@@ -2,14 +2,55 @@ package com.engineers.tubarrio.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 import com.engineers.tubarrio.R;
+import com.engineers.tubarrio.config.Config;
+import com.engineers.tubarrio.entities.User;
+import com.engineers.tubarrio.requests.SendUserInformation;
 
 public class ProfileActivity extends Activity {
+
+    TextView nameTV;
+    TextView neighborhoodTV;
+    TextView publicationsTV;
+    TextView commentsTV;
+    TextView followedTV;
+    ImageView userPicTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        initializeViews();
     }
+
+
+    private void initializeViews(){
+        nameTV = findViewById(R.id.user_name);
+        neighborhoodTV = findViewById(R.id.user_neighborhood);
+        publicationsTV = findViewById(R.id.publications);
+        commentsTV = findViewById(R.id.comments);
+        followedTV = findViewById(R.id.followed);
+        userPicTV = findViewById(R.id.user_profile_picture);
+        User user = Config.getLoggedUserInfo(this);
+        if (!user.firstName.isEmpty())
+            nameTV.setText(user.firstName);
+        publicationsTV.setText(user.cantPublications);
+        commentsTV.setText(user.cantComments);
+        followedTV.setText(user.cantFollowed);
+        
+    }
+
+//    public void newPublicationAction(View view) {
+//        Intent intent = new Intent(ProfileActivity.this, lista de publicaciones);
+//        startActivity(intent);
+//    }
+
 }
