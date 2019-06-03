d2cceb3d5ad58d8087952af0c111ecb9286d0340
Gonzalo Kurin <>
2019-06-03 18:12:27 -0300

    Publication entity and Encoded Image entity

--- TuBarrioApp/app/src/main/java/com/engineers/tubarrio/entities/Publication.java
+++ TuBarrioApp/app/src/main/java/com/engineers/tubarrio/entities/Publication.java
@@ -2,10 +2,18 @@ package com.engineers.tubarrio.entities;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Publication {
 
     private String title;
-    private String fileUrl;
+    private String description;
+    private Double longitude;
+    private Double latitude;
+    private Date updatedOn;
+    private String username;
+    private ArrayList<EncodedImage> images;
 
     public Publication(JSONObject jsonObj) {
     }
@@ -15,14 +23,55 @@ public class Publication {
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
-    public String getFileUrl() {
-        return fileUrl;
+    public String getDescription() {
+        return description;
     }
 
-    public void setFileUrl(String fileUrl) {
-        this.fileUrl = fileUrl;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Date getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public ArrayList<EncodedImage> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<EncodedImage> images) {
+        this.images = images;
     }
 }
