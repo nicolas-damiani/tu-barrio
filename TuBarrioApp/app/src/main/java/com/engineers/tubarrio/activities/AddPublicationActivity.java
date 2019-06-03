3721552c2e62a6b419366860b921de08116ebb6b
Sebastian <srodriguez@moonideas.com>
2019-06-03 18:18:28 -0300

    Scrollable map added

--- TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/AddPublicationActivity.java
+++ TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/AddPublicationActivity.java
@@ -2,6 +2,18 @@ package com.engineers.tubarrio.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.engineers.tubarrio.R;
+import com.engineers.tubarrio.config.Config;
+import com.engineers.tubarrio.entities.User;
+import com.engineers.tubarrio.requests.SendUserInformation;
+
 
 public class AddPublicationActivity extends Activity {
 
