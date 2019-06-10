447b827bed7585d1aec64b5cbea0334ef50bcbb1
Sebastian <srodriguez@moonideas.com>
2019-06-10 19:49:23 -0300

    EncodedImage Test

--- TuBarrioServer/TuBarrio.Entities.Test/ImageTagTestcs.cs
+++ TuBarrioServer/TuBarrio.Entities.Test/ImageTagTestcs.cs
@@ -9,59 +9,28 @@ namespace TuBarrio.Entities.Test
     [TestClass]
     public class ImageTagTestcs
     {
-        public ImageTagTestcs()
+        private ImageTag tag;
+
+        [TestInitialize]
+        public void SetUp()
         {
-            //
-            // TODO: Agregar aquí la lógica del constructor
-            //
+            tag = new ImageTag("tag");
         }
 
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Obtiene o establece el contexto de las pruebas que proporciona
-        ///información y funcionalidad para la serie de pruebas actual.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Atributos de prueba adicionales
-        //
-        // Puede usar los siguientes atributos adicionales conforme escribe las pruebas:
-        //
-        // Use ClassInitialize para ejecutar el código antes de ejecutar la primera prueba en la clase
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup para ejecutar el código una vez ejecutadas todas las pruebas en una clase
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Usar TestInitialize para ejecutar el código antes de ejecutar cada prueba 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup para ejecutar el código una vez ejecutadas todas las pruebas
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
-        public void TestMethod1()
+        public void ImageTagTextConstructorTest()
         {
-            //
-            // TODO: Agregar aquí la lógica de las pruebas
-            //
+            ImageTag secondTag = new ImageTag();
+            secondTag.Text = "tag";
+            Assert.AreEqual(secondTag.Text, tag.Text);
+        }
+
+        [TestMethod]
+        public void ImageTagIdConstructorTest()
+        {
+            tag.Id = 1;
+            Assert.AreEqual(1, tag.Id);
         }
     }
 }
