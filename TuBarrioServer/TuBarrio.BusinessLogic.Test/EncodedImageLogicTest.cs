a405da8603433e1284305b29371004e585d09e74
Sebastian <srodriguez@moonideas.com>
2019-06-10 20:33:07 -0300

    EncodedImageLogic Test

---
+++ TuBarrioServer/TuBarrio.BusinessLogic.Test/EncodedImageLogicTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TuBarrio.Entities;
+using TuBarrio.BusinessLogic;
+using TuBarrio.EntityModels;
+
+namespace TuBarrio.BusinessLogic.Test
+{
+    
+    [TestClass]
+    public class EncodedImageLogicTest
+    {
+        private IEncodedImageLogic imagesLogic;
+        [TestInitialize]
+        public void SetUp()
+        {
+            imagesLogic = new EncodedImageLogic();
+        }
+
+        [TestMethod]
+        public void GetEncodedImageFromModelTest()
+        {
+            EncodedImageModel model = new EncodedImageModel();
+            model.Image = "Image";
+           
+            EncodedImage imageResult = imagesLogic.getEncodedImageFromModel(model);
+            Assert.AreEqual(model.Image, imageResult.Image);
+        }
+    }
+}
