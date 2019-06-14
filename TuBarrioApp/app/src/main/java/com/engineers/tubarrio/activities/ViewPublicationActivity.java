748a35d1e6456dd71e0f919339cae9db88e87e7b
Gonzalo Kurin <>
2019-06-14 20:02:28 -0300

    Subscribe and unsubscribe from publication. Set publication image in view

--- TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/ViewPublicationActivity.java
+++ TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/ViewPublicationActivity.java
@@ -2,6 +2,9 @@ package com.engineers.tubarrio.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +17,8 @@ import com.engineers.tubarrio.R;
 import com.engineers.tubarrio.config.Config;
 import com.engineers.tubarrio.entities.Publication;
 import com.engineers.tubarrio.entities.User;
+import com.engineers.tubarrio.requests.SubscribeUserToPublication;
+import com.engineers.tubarrio.requests.UnsubscribeUserToPublication;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -21,6 +26,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.w3c.dom.Text;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class ViewPublicationActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -31,9 +39,11 @@ public class ViewPublicationActivity extends FragmentActivity implements OnMapRe
     TextView pubUserPhone;
     User user;
     Boolean isAuthor = false;
+    Boolean isSubscribed = false;
     ImageView editIcon;
     ImageView publicationImage;
     Button viewComments;
+    Button followPublication;
     Activity activity;
 
     @Override
@@ -46,6 +56,12 @@ public class ViewPublicationActivity extends FragmentActivity implements OnMapRe
         if(user.getEmail() == publication.getUsername()){
             isAuthor = true;
         }
+
+        //TODO ver si esta subscribed a la publicacion
+        if(false){
+            isSubscribed = true;
+        }
+
         initializeViews();
         InitializeButtons();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -58,19 +74,31 @@ public class ViewPublicationActivity extends FragmentActivity implements OnMapRe
         pubDescription = (TextView) findViewById(R.id.publication_description);
         pubUserPhone = (TextView) findViewById(R.id.publication_user_phone);
         editIcon = (ImageView) findViewById(R.id.edit_publication_icon);
+        publicationImage = (ImageView) findViewById(R.id.publication_image);
         pubTitle.setText(publication.getTitle());
         pubDescription.setText(publication.getDescription());
         pubUserPhone.setText(publication.getUserPhone());
         viewComments = (Button) findViewById(R.id.view_comments_btn);
+        followPublication = (Button) findViewById(R.id.follow_publication_btn);
 
         if(!isAuthor){
             editIcon.setVisibility(View.GONE);
         }
 
         //TODO: Poner la primer imagen como fondo del cosito
+        final InputStream imageStream;
+        try {
+            imageStream = activity.getContentResolver().openInputStream(Uri.parse(publication.getImage()));
+            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+            publicationImage.setImageBitmap(selectedImage);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
-        //TODO: Si ya esta suscripto, boton diga dejar de seguir
+        if(isSubscribed){
+            followPublication.setText("Dejar de seguir");
+        }
     }
 
     @Override
@@ -95,5 +123,12 @@ public class ViewPublicationActivity extends FragmentActivity implements OnMapRe
                 activity.startActivity(goToNextActivity);
             }
         });
+
+        if(isSubscribed){
+            new UnsubscribeUserToPublication(activity, publication);
+        }else{
+            new SubscribeUserToPublication(activity, publication);
+        }
+
     }
 }
