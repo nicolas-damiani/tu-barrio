98b7b3bd984f5ec4173f422d9849885662f4eabd
Gonzalo Kurin <>
2019-06-15 16:45:54 -0300

    Set pin for publications

--- TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/MapsActivity.java
+++ TuBarrioApp/app/src/main/java/com/engineers/tubarrio/activities/MapsActivity.java
@@ -20,6 +20,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -168,7 +169,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Publication publication = mPublications.get(i);
             mMap.addMarker(new MarkerOptions()
                     .position(new LatLng(publication.getLatitude(), publication.getLongitude()))
-                    .title(publication.getTitle()));
+                    .title(publication.getTitle())
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.pin)));
         }
     }
 
