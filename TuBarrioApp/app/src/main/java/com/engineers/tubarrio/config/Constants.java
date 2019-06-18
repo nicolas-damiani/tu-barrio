709b4f84b6763e4e40923dc2a439231d149f8475
Nicolas Damiani <nico_damiani@hotmail.com>
2019-06-18 16:56:51 -0300

    release

--- TuBarrioApp/app/src/main/java/com/engineers/tubarrio/config/Constants.java
+++ TuBarrioApp/app/src/main/java/com/engineers/tubarrio/config/Constants.java
@@ -2,7 +2,7 @@ package com.engineers.tubarrio.config;
 
 public class Constants {
 
-    public static String URL = "http://localhost:56033/";
+    public static String URL = "http://10.47.156.106:56033/";
     public static String MY_PREFERENCES = "TuBarrioMemory";
 
     public static int MY_PUBLICACTIONS = 1;
